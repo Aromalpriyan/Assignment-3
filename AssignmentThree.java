3f9ca4259783f49f0cd749734369bb73ad88144d
Aromalpriyan <aromalpriyan213@gmail.com>
2025-04-07 15:18:35 +0530

    completed

--- AssignmentThree.java
+++ AssignmentThree.java
@@ -1,21 +1,3 @@
-# <p>JAVA ASSIGNMENT<p>
-# <p>03 Handling User Input<p>
-# <p>Let's Practice: Calories Counter Pro<p>
-## <P>**Objective :**<p>
-### <p>In this exercise, you will create a simple Calorie Counter program in Java. The program enables users to input food items and their corresponding calorie values, calculates the total daily calorie intake, and displays the results.<p>
-## <p>**Problem Statement :**<P>
-## <p>Create a Java program that:<p>
-### <p>1. **Prompt the user** to enter the names of three food items.<p>
-### <p>2. **Ask for the calorie values** of each food item.<p>
-### <p>3. **Calculate the total calorie intake.**<p>
-### <p>4. **Displays** a summary of the foods and their corresponding calories.<p>
-## <p>5. Exercise Instructions:<p>
-### <p>6. 1️⃣ **Define the variables** to store food names and calorie values.<p>
-### <p>7.2️⃣ **Use** Scanner **to read input from the user**(both String and int).<p>
-### <p>8.3️⃣ **Handle the** Scanner **properly** by consuming the newline character when needed.<p>
-### <p>9.4️⃣ **Compute the total calorie intake.**<P>
-### <P>10.5️⃣ **Display the results in a clear format.**<P>
-```
 import java.util.Scanner;
 public class AssignmentThree{
     public static void main(String[] args) {
@@ -49,4 +31,3 @@ public class AssignmentThree{
         sc.close();
     }
 }
-```
